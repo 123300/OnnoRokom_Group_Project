9e79e07158f6d3a52098b32d9b6a94c864a5d7b6
Saiful Islam <sajalislam8988@gmail.com>
2022-09-07 10:14:23 +0600

    modified

--- src/Stack Overflow/StackOverflow.Web/Areas/Explorer/Controllers/QuestionController.cs
+++ src/Stack Overflow/StackOverflow.Web/Areas/Explorer/Controllers/QuestionController.cs
@@ -122,6 +122,13 @@ namespace StackOverflow.Web.Areas.Explorer.Controllers
             return View();
         }
 
+        public async Task<IActionResult> AddAnswer(string description,int questionId,int totalVote)
+        {
+            var model = _scope.Resolve<AnswerCreateModel>();
+            await model.AnswerAsync(description, questionId, totalVote);
+            return Ok();
+        }
+
 
     }
 }
---
+++ src/Stack Overflow/StackOverflow.Web/Areas/Explorer/Models/AnswerCreateModel.cs
@@ -0,0 +1,51 @@
+﻿using Autofac;
+using AutoMapper;
+using StackOverflow.Infrastructure.BusinessObjects;
+using StackOverflow.Infrastructure.Services;
+using StackOverflow.Membership.BusinessObjects;
+using StackOverflow.Membership.Services;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.Web.Areas.Explorer.Models
+{
+    public class AnswerCreateModel : ExplorerLayoutModel
+    {
+        private IAnswerService _answerService;
+
+        public string? Description { get; set; }
+        public int QuestionId { get; set; }
+        public Guid? TempId { get; set; }
+        public int? Vote { get; set; }
+        public int? TotalAnsVote { get; set; }
+        public bool IsVoteDone { get; set; }
+        public Question? Question { get; set; }
+        public IList<Comment>? Comments { get; set; }
+
+        public AnswerCreateModel()
+        {
+
+        }
+
+        public AnswerCreateModel(IUserManagerAdapter<ApplicationUser> userManagerAdapter,
+            IHttpContextAccessor httpContextAccessor, IMapper mapper, IAnswerService answerService)
+            : base(userManagerAdapter, httpContextAccessor, mapper)
+        {
+            _answerService = answerService;
+        }
+
+        public override void ResolveDependency(ILifetimeScope scope)
+        {
+            _scope = scope;
+            _answerService = _scope.Resolve<IAnswerService>();
+
+            base.ResolveDependency(scope);
+        }
+
+        internal async Task AnswerAsync(string description, int questionId, int totalVote)
+        {
+            Description = description;
+            QuestionId = questionId;
+            TotalAnsVote = TotalAnsVote;
+        }
+    }
+}
